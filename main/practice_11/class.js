8f943828e372d80ebc8bff62f5f44c92a8d8a1a0
GraceeFlower <1491644103@qq.com>
2020-01-08 01:52:51 +0800

    [WangMin] refactor: encapsulate a function 'isIn' to judge if the student a leader in class

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -7,8 +7,12 @@ class Class {
     return `Class ${this.number}`;
   }
 
+  isIn(student) {
+    return student.klass.number === this.number ? true : false;
+  }
+
   assignLeader(student) {
-    if (student.klass.number === this.number) {
+    if (this.isIn(student)) {
       this.leader = student;
       if (this.leaderListener) {
         this.leaderListener.callWithLeader(student);
